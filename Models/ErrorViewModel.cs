86bbd4137532d1519ce84323e7599a6e8a76ca27
Jean Paul Tabja <jptabja@hotmail.com>
2020-05-02 12:15:14 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SecondAKSWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
